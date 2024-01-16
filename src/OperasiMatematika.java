969d244b50d8cd6cfdc607e5cde86e53e4b5feae
Ahmad Dzulfikar <dzulfikar19.ahmad@gmail.com>
2024-01-16 10:00:42 +0700

    operasi matematika

---
+++ src/OperasiMatematika.java
@@ -0,0 +1,45 @@
+public class OperasiMatematika {
+    public static void main(String[] args) {
+
+        // Operasi Matematika biasa
+        int a = 10;
+        int b = 5;
+
+        System.out.println(a + b );
+        System.out.println(a - b );
+        System.out.println(a * b );
+        System.out.println(a / b );
+        System.out.println(a % b );
+
+
+        int c = 100;
+
+        c += 10;
+        System.out.println(c);
+
+        c -= 10;
+        System.out.println(c);
+
+        c *= 10;
+        System.out.println(c);
+
+        String amba = "nasi goreng";
+        System.out.println(amba);
+        
+        // Unary
+        int d = 10;
+        int e = -5;
+
+        d++;
+        System.out.println(d);
+
+        d--;
+        System.out.println(d);
+
+        e--;
+        System.out.println(e);
+
+        System.out.println(!true);
+        System.err.println(!false);
+    }
+}
---
+++ src/OperasiPerbandingan.java
@@ -0,0 +1,8 @@
+public class OperasiPerbandingan {
+    public static void main(String[] args) {
+        int value1 = 101;
+        int value2 = 100;
+
+        
+    }
+}
